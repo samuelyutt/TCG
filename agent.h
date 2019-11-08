78da92a50ad085c25a9009864b8bf0436af62a14
Samuel Yu <samuelyuyy@gmail.com>
2019-11-08 21:36:32 +0800

    Modify as HW1

--- agent.h
+++ agent.h
@@ -10,49 +10,52 @@
 #include "weight.h"
 #include <fstream>
 
+int operation;
+std::vector<board::cell> bag;
+
 class agent {
 public:
-	agent(const std::string& args = "") {
-		std::stringstream ss("name=unknown role=unknown " + args);
-		for (std::string pair; ss >> pair; ) {
-			std::string key = pair.substr(0, pair.find('='));
-			std::string value = pair.substr(pair.find('=') + 1);
-			meta[key] = { value };
-		}
-	}
-	virtual ~agent() {}
-	virtual void open_episode(const std::string& flag = "") {}
-	virtual void close_episode(const std::string& flag = "") {}
-	virtual action take_action(const board& b) { return action(); }
-	virtual bool check_for_win(const board& b) { return false; }
+    agent(const std::string& args = "") {
+        std::stringstream ss("name=unknown role=unknown " + args);
+        for (std::string pair; ss >> pair; ) {
+            std::string key = pair.substr(0, pair.find('='));
+            std::string value = pair.substr(pair.find('=') + 1);
+            meta[key] = { value };
+        }
+    }
+    virtual ~agent() {}
+    virtual void open_episode(const std::string& flag = "") {}
+    virtual void close_episode(const std::string& flag = "") {}
+    virtual action take_action(const board& b) { return action(); }
+    virtual bool check_for_win(const board& b) { return false; }
 
 public:
-	virtual std::string property(const std::string& key) const { return meta.at(key); }
-	virtual void notify(const std::string& msg) { meta[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
-	virtual std::string name() const { return property("name"); }
-	virtual std::string role() const { return property("role"); }
+    virtual std::string property(const std::string& key) const { return meta.at(key); }
+    virtual void notify(const std::string& msg) { meta[msg.substr(0, msg.find('='))] = { msg.substr(msg.find('=') + 1) }; }
+    virtual std::string name() const { return property("name"); }
+    virtual std::string role() const { return property("role"); }
 
 protected:
-	typedef std::string key;
-	struct value {
-		std::string value;
-		operator std::string() const { return value; }
-		template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
-		operator numeric() const { return numeric(std::stod(value)); }
-	};
-	std::map<key, value> meta;
+    typedef std::string key;
+    struct value {
+        std::string value;
+        operator std::string() const { return value; }
+        template<typename numeric, typename = typename std::enable_if<std::is_arithmetic<numeric>::value, numeric>::type>
+        operator numeric() const { return numeric(std::stod(value)); }
+    };
+    std::map<key, value> meta;
 };
 
 class random_agent : public agent {
 public:
-	random_agent(const std::string& args = "") : agent(args) {
-		if (meta.find("seed") != meta.end())
-			engine.seed(int(meta["seed"]));
-	}
-	virtual ~random_agent() {}
+    random_agent(const std::string& args = "") : agent(args) {
+        if (meta.find("seed") != meta.end())
+            engine.seed(int(meta["seed"]));
+    }
+    virtual ~random_agent() {}
 
 protected:
-	std::default_random_engine engine;
+    std::default_random_engine engine;
 };
 
 /**
@@ -60,43 +63,43 @@ protected:
  */
 class weight_agent : public agent {
 public:
-	weight_agent(const std::string& args = "") : agent(args) {
-		if (meta.find("init") != meta.end()) // pass init=... to initialize the weight
-			init_weights(meta["init"]);
-		if (meta.find("load") != meta.end()) // pass load=... to load from a specific file
-			load_weights(meta["load"]);
-	}
-	virtual ~weight_agent() {
-		if (meta.find("save") != meta.end()) // pass save=... to save to a specific file
-			save_weights(meta["save"]);
-	}
+    weight_agent(const std::string& args = "") : agent(args) {
+        if (meta.find("init") != meta.end()) // pass init=... to initialize the weight
+            init_weights(meta["init"]);
+        if (meta.find("load") != meta.end()) // pass load=... to load from a specific file
+            load_weights(meta["load"]);
+    }
+    virtual ~weight_agent() {
+        if (meta.find("save") != meta.end()) // pass save=... to save to a specific file
+            save_weights(meta["save"]);
+    }
 
 protected:
-	virtual void init_weights(const std::string& info) {
-		net.emplace_back(65536); // create an empty weight table with size 65536
-		net.emplace_back(65536); // create an empty weight table with size 65536
-		// now net.size() == 2; net[0].size() == 65536; net[1].size() == 65536
-	}
-	virtual void load_weights(const std::string& path) {
-		std::ifstream in(path, std::ios::in | std::ios::binary);
-		if (!in.is_open()) std::exit(-1);
-		uint32_t size;
-		in.read(reinterpret_cast<char*>(&size), sizeof(size));
-		net.resize(size);
-		for (weight& w : net) in >> w;
-		in.close();
-	}
-	virtual void save_weights(const std::string& path) {
-		std::ofstream out(path, std::ios::out | std::ios::binary | std::ios::trunc);
-		if (!out.is_open()) std::exit(-1);
-		uint32_t size = net.size();
-		out.write(reinterpret_cast<char*>(&size), sizeof(size));
-		for (weight& w : net) out << w;
-		out.close();
-	}
+    virtual void init_weights(const std::string& info) {
+        net.emplace_back(65536); // create an empty weight table with size 65536
+        net.emplace_back(65536); // create an empty weight table with size 65536
+        // now net.size() == 2; net[0].size() == 65536; net[1].size() == 65536
+    }
+    virtual void load_weights(const std::string& path) {
+        std::ifstream in(path, std::ios::in | std::ios::binary);
+        if (!in.is_open()) std::exit(-1);
+        uint32_t size;
+        in.read(reinterpret_cast<char*>(&size), sizeof(size));
+        net.resize(size);
+        for (weight& w : net) in >> w;
+        in.close();
+    }
+    virtual void save_weights(const std::string& path) {
+        std::ofstream out(path, std::ios::out | std::ios::binary | std::ios::trunc);
+        if (!out.is_open()) std::exit(-1);
+        uint32_t size = net.size();
+        out.write(reinterpret_cast<char*>(&size), sizeof(size));
+        for (weight& w : net) out << w;
+        out.close();
+    }
 
 protected:
-	std::vector<weight> net;
+    std::vector<weight> net;
 };
 
 /**
@@ -104,14 +107,14 @@ protected:
  */
 class learning_agent : public agent {
 public:
-	learning_agent(const std::string& args = "") : agent(args), alpha(0.1f) {
-		if (meta.find("alpha") != meta.end())
-			alpha = float(meta["alpha"]);
-	}
-	virtual ~learning_agent() {}
+    learning_agent(const std::string& args = "") : agent(args), alpha(0.1f) {
+        if (meta.find("alpha") != meta.end())
+            alpha = float(meta["alpha"]);
+    }
+    virtual ~learning_agent() {}
 
 protected:
-	float alpha;
+    float alpha;
 };
 
 /**
@@ -122,22 +125,39 @@ protected:
  */
 class rndenv : public random_agent {
 public:
-	rndenv(const std::string& args = "") : random_agent("name=random role=environment " + args),
-		space({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }), popup(0, 9) {}
+    rndenv(const std::string& args = "") : random_agent("name=random role=environment " + args),
+        space({ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }), popup(0, 9) {}
 
-	virtual action take_action(const board& after) {
-		std::shuffle(space.begin(), space.end(), engine);
-		for (int pos : space) {
-			if (after(pos) != 0) continue;
-			board::cell tile = popup(engine) ? 1 : 2;
-			return action::place(pos, tile);
-		}
-		return action();
-	}
+    virtual action take_action(const board& after) {
+        if (bag.empty()) {
+            for (int i = 1; i <= 3; i++)
+                bag.push_back(i);
+            std::random_shuffle(bag.begin(), bag.end());
+        }
+        board::cell tile = bag.back();
+        bag.pop_back();
+
+        std::vector<int> legalspace;
+        switch (operation) {
+            case 0: legalspace = {12, 13, 14, 15}; break;
+            case 1: legalspace = {0, 4, 8, 12}; break;
+            case 2: legalspace = {0, 1, 2, 3}; break;
+            case 3: legalspace = {3, 7, 11, 15}; break;
+            default: legalspace = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        }
+
+        std::shuffle(legalspace.begin(), legalspace.end(), engine);
+        
+        for (int pos : legalspace) {
+            if (after(pos) != 0) continue;
+            return action::place(pos, tile);
+        }
+        return action();
+    }
 
 private:
-	std::array<int, 16> space;
-	std::uniform_int_distribution<int> popup;
+    std::array<int, 16> space;
+    std::uniform_int_distribution<int> popup;
 };
 
 /**
@@ -146,18 +166,23 @@ private:
  */
 class player : public random_agent {
 public:
-	player(const std::string& args = "") : random_agent("name=dummy role=player " + args),
-		opcode({ 0, 1, 2, 3 }) {}
+    player(const std::string& args = "") : random_agent("name=dummy role=player " + args),
+        opcode({ 0, 1, 2, 3 }) {}
 
-	virtual action take_action(const board& before) {
-		std::shuffle(opcode.begin(), opcode.end(), engine);
-		for (int op : opcode) {
-			board::reward reward = board(before).slide(op);
-			if (reward != -1) return action::slide(op);
-		}
-		return action();
-	}
+    virtual action take_action(const board& before) {
+        board::reward bestreward = -1;
+        int bestop = 0;
+        for (int op = 0; op < 4; op++) {
+            board::reward reward = board(before).slide(op);
+            if (reward > bestreward) {
+                bestreward = reward;
+                bestop = op;
+            }
+        }
+        if (bestreward != -1) return action::slide(operation = bestop);
+        return action();
+    }
 
 private:
-	std::array<int, 4> opcode;
+    std::array<int, 4> opcode;
 };
